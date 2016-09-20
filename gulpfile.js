76cb536ba7d641925b377ced4fd5fcd3e874490f
Daniel Griffiths <daniel.griffiths.jobs@gmail.com>
2016-09-20 20:22:47 +0100

    Added gulp

---
+++ gulpfile.js
@@ -0,0 +1,60 @@
+/*  
+|-------------------------------------------------------------------------- 
+| Include Dependencies
+|--------------------------------------------------------------------------
+|
+*/
+
+var gulp = require('gulp');
+var watch = require('gulp-watch');
+var stylus = require('gulp-stylus');
+var sourcemaps = require('gulp-sourcemaps');
+
+/*  
+|-------------------------------------------------------------------------- 
+| Variables
+|--------------------------------------------------------------------------
+|
+*/
+
+/* css */
+var css_source = './resources/assets/css/app.styl';
+var css_dest = './public/assets/css';
+ 
+/*  
+|-------------------------------------------------------------------------- 
+| Watch
+|--------------------------------------------------------------------------
+|
+*/
+
+gulp.task('watch', function() {
+  gulp.watch(css_source, ['compile-css']);
+});
+
+/*  
+|-------------------------------------------------------------------------- 
+| Compile Stylus
+|--------------------------------------------------------------------------
+|
+*/
+
+gulp.task('compile-css', function () {
+  return gulp.src(css_source)
+  	.pipe(sourcemaps.init())
+    .pipe(stylus({
+      compress: true
+    }))
+    .pipe(sourcemaps.write())
+    .pipe(gulp.dest(css_dest));
+});
+ 
+/*  
+|-------------------------------------------------------------------------- 
+| Register Default Task
+|--------------------------------------------------------------------------
+|
+*/
+
+gulp.task('default', ['compile-css']);
+ 
