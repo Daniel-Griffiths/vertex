d446034b1a9dd6b9517ad5eb0f8b357d6b26fa20
Daniel Griffiths <daniel.griffiths.jobs@gmail.com>
2017-05-03 20:57:59 +0100

    Made example view work on both http and https

--- app/resources/views/layout.blade.php
+++ app/resources/views/layout.blade.php
@@ -4,7 +4,7 @@
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<title>{{ $title }}</title>
-	<link href="http://fonts.googleapis.com/css?family=Lato:100" rel="stylesheet" type="text/css">
+	<link href="//fonts.googleapis.com/css?family=Lato:100" rel="stylesheet" type="text/css">
 	<link rel="stylesheet" href="/assets/css/app.css">
 </head>
 <body>
