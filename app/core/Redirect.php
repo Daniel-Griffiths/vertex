472765eb7cb157cc70ad01570d7d5a7069beb922
Daniel Griffiths <daniel.griffiths.jobs@gmail.com>
2017-06-02 23:47:48 +0100

    Add redirect back method (Http class now called Redirect)

---
+++ app/core/Redirect.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Vertex\Core;
+
+class Redirect
+{
+    /**
+     * Redirect to the specified url.
+     * 
+     * @param  string $url 
+     * @return void
+     */
+    public static function to($url)
+    {
+		self::createRedirect($url);
+    }
+
+    /**
+     * Redirect to the previous url.
+     * 
+     * @return void
+     */
+    public static function back()
+    {
+        self::createRedirect($_SERVER['HTTP_REFERER']);
+    }
+
+    /**
+     * Create a new redirect.
+     * 
+     * @param  string $location 
+     * @return void
+     */
+    public static function createRedirect($location)
+    {
+        header('location:' . $location);
+        die;	
+    }
+}
