a79eb92a9dc201e4bcba79d32eb88700c99b4d30
Daniel Griffiths <daniel.griffiths.jobs@gmail.com>
2017-06-13 07:41:30 +0100

    Add redirect test

---
+++ tests/RedirectTest.php
@@ -0,0 +1,15 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+use Vertex\Core\Redirect;
+
+class RedirectTest extends TestCase
+{
+	/**
+     * @runInSeparateProcess
+     */
+    public function testRedirectTo()
+    {
+        Redirect::to('http://google.com');
+    }
+}
