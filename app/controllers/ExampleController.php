68eeac3d574ba7e3e316a0a4d6b7d06f7af791a6
Daniel Griffiths <daniel.griffiths.jobs@gmail.com>
2017-08-05 12:17:07 +0100

    Add helpers

--- app/controllers/ExampleController.php
+++ app/controllers/ExampleController.php
@@ -4,13 +4,12 @@ namespace Vertex\Controller;
 
 use Vertex\Model\ExampleModel;
 use Vertex\Core\Controller;
-use Vertex\Core\View;
 
 class ExampleController extends Controller
 {
     public function index()
     {
-        return View::render('examples.homepage', [
+        return view('examples.homepage', [
             'title' => 'Vertex',
             'tagline' => 'Build Something Amazing'
         ]);
---
+++ app/core/Helpers.php
@@ -0,0 +1,57 @@
+<?php 
+
+use Vertex\Core\View;
+use Vertex\Core\Redirect;
+
+/**
+ * Dump the specified variables and end the script.
+ * 
+ * @param  mixed $data 
+ * @return void
+ */
+if(!function_exists('dd')){
+	function dd(...$data)
+	{
+		var_dump(...$data);
+		die;
+	}
+}
+
+/**
+ * Renders a view.
+ * 
+ * @param  string $view       
+ * @param  array  $parameters 
+ * @return \Jenssegers\Blade\Blade
+ */
+if(!function_exists('view')){
+	function view(string $view, array $parameters = [])
+	{
+        return View::render($view, $parameters);
+	}
+}
+
+/**
+ * Redirect to the specified url.
+ * 
+ * @param  string $url 
+ * @return void
+ */
+if(!function_exists('redirect')){
+	function redirect($url)
+	{
+        return Redirect::to($url);
+	}
+}
+
+/**
+ * Redirect to the previous page.
+ * 
+ * @return void
+ */
+if(!function_exists('back')){
+	function back()
+	{
+        return Redirect::back();
+	}
+}
