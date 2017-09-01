5ea6c9a2e4b094f81adf9ca5409c28e6cccc1a44
Daniel Griffiths <daniel.griffiths.jobs@gmail.com>
2017-09-01 09:20:39 +0100

    Change quotes

--- app/core/Database.php
+++ app/core/Database.php
@@ -17,7 +17,7 @@ class Database
     protected $options = [
         PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
         PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_OBJ,
-        PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"
+        PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8'
     ];
 
     /**
